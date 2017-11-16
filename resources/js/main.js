dd6d86c60e5ec342890ff21fbedb39ebd4bb5cba
wthompson21 <wthompson@youronegiantleap.com>
2017-11-15 23:06:32 -0800

    jQuery Carousel Working.

---
+++ resources/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+});
